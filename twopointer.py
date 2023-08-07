38065f4f5fc6a1bd6246923743f5e70c6ee6a0e9
Hong Tang <53389274+stanghong@users.noreply.github.com>
2023-08-06 23:26:32 -0700

    Update twopointer.py

--- twopointer.py
+++ twopointer.py
@@ -16,3 +16,31 @@ class Solution:
             else:
                 r-=1
         return res
+
+# 42. Trapping Rain Water
+# difficulty is cannot think about this algo O(N) time and space
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        #algo min(l, r)-h[i]
+        if len(height)==1: return 0
+
+        maxleft=[0]*len(height)
+        maxright=[0]*len(height)
+        
+        maxl=0
+
+        for l in range(len(height)):
+            maxl=max(maxl, height[l])
+            maxleft[l]=maxl
+
+        maxr=0
+        for r in range(len(height)-1,-1, -1):
+            maxr=max(maxr, height[r])
+            maxright[r]=maxr
+        
+        vol=0
+        for i in range(len(height)):
+            vol+=min(maxleft[i], maxright[i])-height[i]
+        
+        return vol
