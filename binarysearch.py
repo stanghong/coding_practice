e01d382c5fc0b8eb43c6e77bf2f4d56d32bdd53a
Hong Tang <53389274+stanghong@users.noreply.github.com>
2023-08-12 10:10:23 -0700

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -20,3 +20,33 @@ class Solution:
                         r -= 1
         
         return False
+
+# 875. Koko Eating Bananas
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        #count pile[i]//k = hr hr<=8 for mini hr
+        # k=[1, sum[bananas] if eat one] bananas can scan 
+        # find the target sum(hr)<= h--> binary search
+
+        totBan=max(piles)
+        l, r =1, totBan
+        res=max(piles)
+
+        while l <= r:
+            m= (l+r)//2
+           
+            hrEat = 0
+            for e in piles:
+                hrEat+=math.ceil(e/m)
+
+            # learned the stop criteria res = min(res, m) for selecting min, max
+            # move from left so the res decreasing find minimum; from to right find max
+
+            if hrEat <= h: # too slow
+                res = min(res, m)
+                r = m - 1
+            else: # too fast
+                l = m + 1
+
+        return res
+
