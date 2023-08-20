4a16c822487c500659af5e645e53eae81b42dc95
Hong Tang <53389274+stanghong@users.noreply.github.com>
2023-08-19 18:13:30 -0700

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,6 +1,6 @@
 # 206. Reverse Linked List
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        #iteratively two pointer pre
+        #iteratively two pointer pre, yellow
         #     1->2->3
         #pre  cur
         pre = None
@@ -11,4 +11,20 @@
             pre = cur
             cur = temp
         return pre
+
+# recursion: not required, 
+        # note: cur.next.next = cur and cur.next=None 
+        # and learn how to write recursion 
+        if head ==None or head.next==None:
+            return head
+
+        def flip(cur):
+            if not cur.next:
+                return cur
+            newhead=flip(cur.next)
+            cur.next.next =cur
+            cur.next=None
+            return newhead
+
+        return flip(head)
         
