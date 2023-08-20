023020fc3d12019c73b40ec9c97918210a443693
Hong Tang <53389274+stanghong@users.noreply.github.com>
2023-08-19 19:14:39 -0700

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -27,4 +27,24 @@
             return newhead
 
         return flip(head)
+
+# 21. Merge Two Sorted Lists
         
+       dummy=ListNode()
+        cur=dummy
+
+        while list1 and list2:
+            if list1.val<=list2.val:
+                cur.next =ListNode(list1.val)
+                list1=list1.next
+            else:
+                cur.next =ListNode(list2.val)
+                list2=list2.next
+            cur=cur.next
+        
+        if list1:
+            cur.next=list1
+        if list2:
+            cur.next=list2
+            
+        return dummy.next
