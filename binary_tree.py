13c260a89b3419d4ce172417da53c2eb406c8b8d
Hong Tang <53389274+stanghong@users.noreply.github.com>
2023-08-27 19:25:47 -0700

    Create binary_tree.py

---
+++ binary_tree.py
@@ -0,0 +1,15 @@
+# reverse order
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root: return None
+
+        q=deque([root])
+        qsize=len(q)
+
+        # for _ in range(qsize):
+        while q:
+            cur=q.popleft()
+            cur.left, cur.right =cur.right, cur.left
+            if cur.left: q.append(cur.left)
+            if cur.right: q.append(cur.right)
+        
+        return root
