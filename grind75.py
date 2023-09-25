c20ef5943a16764cd49ba0941f9cdf51978fe28a
Hong Tang <53389274+stanghong@users.noreply.github.com>
2023-09-25 10:57:07 -0700

    Update grind75.py

--- grind75.py
+++ grind75.py
@@ -13,3 +13,35 @@ class Solution:
                 res[-1][1]=max(res[-1][1], interval[0], interval[1])
 
         return res
+
+# 57. Insert Interval
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        #if inserted newInterval, just insert interval
+        # if no overlap insert directly left
+        # no overlap right, insert newInterval check if not inserted
+        # if overlapped, updated newInterval left and right
+      
+        res=[]
+        inserted=False
+        for interval in intervals:
+
+            if inserted: 
+                 res.append(interval)
+            elif interval[1] < newInterval[0]: #no overlap 
+                res.append(interval)
+            elif interval[0]>newInterval[1]: #no overlap 
+                if not inserted: 
+                    res.append(newInterval)
+                    inserted=True
+                res.append(interval)
+            else: #hasOverlap
+                newInterval[0]=min(interval[0],newInterval[0] )
+                newInterval[1]=max(interval[1],newInterval[1] )
+                # res.append(newInterval)
+
+        if not inserted: # incase intervals is empty
+            res.append(newInterval)      
+
+        
+        return res
